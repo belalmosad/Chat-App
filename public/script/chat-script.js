e8692cd0a765340f4ca7edc4fb97d9e8283f1f49
unknown <belalelmeligy516@gmail.com>
2022-06-02 21:25:22 +0200

    renamed to chat script and added handling evens with socket

---
+++ public/script/chat-script.js
@@ -0,0 +1,38 @@
+let socket = io();
+
+let messageBox = document.getElementById('msg');
+let messageBody = document.getElementById('msginpt');
+let userName = "llll";
+let msgObj = {'username':userName, 'msg': messageBody.value}
+messageBox.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    if(messageBody.value) {
+        socket.emit('new message', msgObj);
+        messageBody.value = '';
+    }
+});
+
+socket.on('new message', (msgObj) => {
+    let p = document.createElement('p');
+    if(msgObj.username == userName) {
+        p.innerHTML = `${msgObj.username} says: ` +  msgObj.msg;
+    }
+    else {
+        p.innerHTML = "xUUU";
+    }
+    
+    
+    document.body.appendChild(p);
+});
+
+socket.on('new user', (username) => {
+    let newUserDiv = document.createElement('div');
+    newUserDiv.classList.add('joined');
+    newUserDiv.innerHTML = `${username} joined conversation.`;
+    document.body.appendChild(newUserDiv);
+    msgObj.username = "ffffffffffffffffff";
+} );
+
+
+
